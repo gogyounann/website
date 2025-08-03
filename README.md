6bdc15392d7273ab3c943b42e275421705296bfe
gogyounann <gogyounann@yahoo.co.jp>
2025-08-03 11:37:08 +0900

    20250803

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # website
-Webサイト
+
+Web サイト(weimei)
--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,20 @@ particlesJS.load('particles-js', 'assets/js/particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
 
+const form = document.getElementById('contactForm');
+const dialog = document.getElementById('successDialog');
+const closeBtn = document.getElementById('closeDialog');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // 送信のデフォルト動作を止める
+
+    form.reset(); // フォームをリセット
+
+    dialog.showModal(); // モーダルを開く
+});
+
+closeBtn.addEventListener('click', () => {
+    dialog.close(); // モーダルを閉じる
+});
+
 
